1def6fcd161cc722d9d99235214ff1dd06495787
Manuel Pino Ros <manuelpinoros@gmail.com>
2025-05-27 10:16:11 +0200

    Initial Commit

---
+++ RickStar/RickStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickStarApp.swift
+//  RickStar
+//
+//  Created by Manuel Pino Ros on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RickStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
